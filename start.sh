3bcd98e79cba5baa96a7914d014e7a57543673d3
DMC <surendra.singh20.in@gmail.com>
2024-12-19 08:28:51 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,32 +1,12 @@
 #!/bin/bash
 
+. ~/.bash_profile
+
 module_name="lm_hlr"
 main_module="list_management" #keep it empty "" if there is no main module 
 log_level="INFO" # INFO, DEBUG, ERROR
 
 ########### DO NOT CHANGE ANY CODE OR TEXT AFTER THIS LINE #########
-
-. ~/.bash_profile
-
- if [ "${main_module}" == "" ]
-  then
-     build_path="${APP_HOME}/${module_name}_module"
-     log_path="${LOG_HOME}/${module_name}_module"
-  else
-     if [ "${main_module}" == "utility" ] || [ "${main_module}" == "api_service" ] || [ "${main_module}" == "gui" ]
-     then
-       build_path="${APP_HOME}/${main_module}/${module_name}"
-       log_path="${LOG_HOME}/${main_module}/${module_name}"
-     else
-       build_path="${APP_HOME}/${main_module}_module/${module_name}"
-       log_path="${LOG_HOME}/${main_module}_module/${module_name}"
-     fi
-  fi
-
-cd ${build_path}/script
-
-  echo "Starting process module for all operator....."
-
 
 ## Start for cellcard operator ##
   ./start.sh CC
