28eaa8ac287b538ee2c06e50d2b4d1e4006c394e
DMC <surendra.singh20.in@gmail.com>
2025-01-08 12:16:52 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,19 +13,18 @@ echo "Starting process module for all operator....."
 cd ./script/
 ## Start for cellcard operator ##
    mkdir -p  ${log_path}/cellcard
-   ./script.sh cc 1>/dev/null 2>${log_path}/cellcard/${module_name}.error &
+   ./script.sh cc 1>${log_path}/cellcard/${module_name}_script.log 2>${log_path}/cellcard/${module_name}.error &
 
 ## Start for smart operator ##
    mkdir -p  ${log_path}/smart
-  ./script.sh sm 1>/dev/null 2>${log_path}/smart/${module_name}.error &
+  ./script.sh sm 1>${log_path}/smart/${module_name}_script.log 2>${log_path}/smart/${module_name}.error &
 
 ## Start for seatel operator ##
     mkdir -p  ${log_path}/seatel
-   ./script.sh st 1>/dev/null 2>${log_path}/seatel/${module_name}.error &
+   ./script.sh st 1>${log_path}/seatel/${module_name}_script.log 2>${log_path}/seatel/${module_name}.error &
 
 ## Start for metfone operator ##
    mkdir -p  ${log_path}/metfone
-  ./script.sh vt 1>/dev/null 2>${log_path}/metfone/${module_name}.error &
-
+  ./script.sh vt 1>${log_path}/metfone/${module_name}_script.log 2>${log_path}/metfone/${module_name}.error &
 
 
